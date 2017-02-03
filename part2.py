ed6abb934591b6a5e678dd3c331fe135eef7b272
Michael Golfi <Michael Golfi>
2017-02-03 02:03:55 -0500

    Added base sol files

---
+++ part2.py
@@ -0,0 +1,9 @@
+"""
+COMP 551 - Assignment 2
+Text Classification
+
+Authors:
+    Michael Golfi <michael.golfi@mail.mcgill.ca>
+    Shruti Bhanderi <shruti.bhanderi@mail.mcgill.ca>
+    Zahra Khambaty <zahra.khambaty@mail.mcgill.ca>
+"""
